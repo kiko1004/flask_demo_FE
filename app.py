4a6d2569c59d7f50c67bd0bd7f74a98985b3eb8a
Kiril Spiridonov <kirilspiridonov@abv.bg>
2024-09-18 19:38:36 +0300

    added html

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     args = request.args
-    return render_template("index.html", name=args.get("name"), city="Sofia")
+    return render_template("index.html")
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5888)
